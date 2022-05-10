2d236504e549ffc017221d9f8a1e8384fa62be79
dzjuband <dzjuband@gmail.com>
2022-05-10 11:01:38 +0300

    upd

--- script.js
+++
@@ -1,6 +0,0 @@
-const btnmenu = document.querySelector('.header__btn-menu');
-
-document.querySelector('.header__btn-menu').addEventListener('click',function(){
-    document.querySelector('.header__btn-menu').classList.toggle('header__btn-menu--open');
-    document.querySelector('.header__nav').classList.toggle("header__nav--open")
-})
